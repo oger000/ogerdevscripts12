cb55e5b8fdb4e4f4a78485b85ef5573a83165df6
oger000 <gerhard.oettl@ogersoft.at>
2014-08-17 12:00:44 +0200

    mre aliases

--- ext5aliases.sh
+++ ext5aliases.sh
@@ -2,25 +2,46 @@
 
 
 FILE_IN="ext-all-debug.js"
-FILE_OUT="aliases.localonly"
+FILE_OUT="ext5aliases.localonly"
 
 rm -f $FILE_OUT
 
-rm -f localonly1
-grep -P "alias\s*:" $FILE_IN >> localonly1
-grep -P "xtype\s*:" $FILE_IN >> localonly1
 
-cat localonly1  | sed -e "s/\[//" > localonly2a
-cat localonly2a | sed -e "s/\]//" > localonly2b
-cat localonly2b | sed -e "s/^ *alias *: *//" > localonly2c
-cat localonly2c | sed -e "s/^ *xtype *: *'/'widget./" > localonly2d
-cat localonly2d | sed -e "s/^ *xtype *: */'widget./" > localonly2z
+rm -f localonly1[a-z]
 
-cat localonly2z | sort | uniq > localonly3
+grep -P "alias\s*:" $FILE_IN >> localonly1a
+grep -P "xtype\s*:" $FILE_IN >> localonly1a
+grep -Pzo '(?ms)alias\s*:\s*\[.*?\]' $FILE_IN >> localonly1a
 
 
+rm -f localonly2[a-z]
+cat localonly1a > localonly2a
 
-#rm -f localonly1 localonly2[a-z]
+cat localonly2a | sed -e "s/\[//" > localonly2b
+cat localonly2b | sed -e "s/\]//" > localonly2c
+cat localonly2c | sed -e "s/^ *alias *: *//" > localonly2d
+cat localonly2d | sed -e "s/^ *xtype *: *'/'widget./" > localonly2e
+cat localonly2e | sed -e "s/^ *//" > localonly2f
+cat localonly2f | sed -e "s/ *\$//" > localonly2g
+
+
+rm -f localonly3[a-z]*
+cat localonly2g > localonly3a
+
+cat localonly3a | grep "xtype:" >> localonly3b-del
+cat localonly3a | grep "throw new" >> localonly3b-del
+cat localonly3a | sed -e "/xtype:/d" > localonly3aa
+cat localonly3aa | sed -e "/throw new/d" > localonly3b
+
+cat localonly3b | sort | uniq > localonly3c
+cat localonly3c | sed -e "/^\$/d" > localonly3d
+cat localonly3d | sed -e "s/'\$/',/" > localonly3e
+
+
+cat localonly3e > $FILE_OUT
+
+
+#rm -f localonly1[a-z] localonly2[a-z]
 
 
 
