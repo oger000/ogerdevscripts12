0fad346e19223b4490e9a4f31bb1006896533a59
oger000 <gerhard.oettl@ogersoft.at>
2022-03-15 19:57:56 +0100

    format xfdf

--- xfdf.php
+++ xfdf.php
@@ -10,17 +10,17 @@ error_reporting((E_ALL | E_STRICT));
 error_reporting(error_reporting() ^ E_NOTICE);
 
 $scriptName = array_shift($argv);
-$defFileName = array_shift($argv);
+$fieldsFileName = array_shift($argv);
 
-if (!file_exists($defFileName)) {
-  echo "*** Cannot find fdf definition file '{$defFile}'.\n";
+if (!file_exists($fieldsFileName)) {
+  echo "*** Cannot find fdf definition file '{$fieldsFileName}'.\n";
   exit;
 }
 
 
 $def = array();
 $allDefs = array();
-$lines = file($defFileName);
+$lines = file($fieldsFileName);
 foreach ($lines as $line) {
 
   if (substr($line, 0, 3) == "---") {
@@ -47,10 +47,22 @@ if ($def) {
   $allDefs['fdf'] = $def;
 }
 
-$extPos = strrpos($defFileName, ".");
+$extPos = strrpos($fieldsFileName, ".");
 if ($extPos === false) {
-  $extPos  = strlen($defFileName);
+  $extPos  = strlen($fieldsFileName);
 }
 
-$outFileName = substr($defFileName, 0, $extPos) . ".def_arr";
-file_put_contents($outFileName, var_export($allDefs, true));
+ksort($allDefs);
+
+$outText = var_export($allDefs, true);
+// file_put_contents("x", $outText);exit;
+
+$outText = preg_replace("/^\s+/m", "", $outText);
+$outText = str_replace("\n", "", $outText) . "\n";
+$outText = str_replace("),", "),\n  ", $outText);
+$arr = explode("(", $outText, 2);
+$outText = implode("(\n  ", $arr);
+
+$outFileName = $fieldsFileName . ".inc.php";
+echo "write $outFileName\n";
+file_put_contents($outFileName, $outText);
